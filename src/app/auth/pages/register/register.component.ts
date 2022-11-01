cabe1833756fbba754dada17b5c9798ad142dce5
Leyder Quintero <lele.quinter@gmail.com>
2022-10-31 20:37:05 -0500

    Pantalla registro y login

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,11 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    name: ['Test 4', [Validators.required, Validators.minLength(3)]],
+    email: ['test4@test.com', [Validators.required, Validators.email]],
+    password: ['123123', [Validators.required, Validators.minLength(6)]],
+  })
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+
+  register(){
+    console.log(this.miFormulario.value);
+    console.log(this.miFormulario.valid);
   }
 
 }
